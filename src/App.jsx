4527fd6e6f19b3144a726e5cb0b574e26af7c265
Kurtko10 <kurtkoaf@gmail.com>
2024-04-28 04:54:29 +0200

    Inicio proyecto front

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Header from './components/Header/Header'
+import { Body } from './pages/Body/Body'
+
+
+//-------------------
+function App() {
+
+
+  return (
+    <>
+
+      <Header />
+      <Body/>
+
+    </>
+  );
+}
+
+export default App
---
+++ src/pages/Body/Body.jsx
@@ -0,0 +1,30 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+
+
+import { Home } from "../Home/Home"
+
+
+import "./Body.css"
+
+
+//-----------------------
+
+
+export const Body = () =>{
+
+
+    
+    return(
+        <>
+            <Routes>
+                <Route path="*" element={<Navigate to="/"/>}/>
+                <Route path="/" element={<Home />}/>
+
+
+
+
+            </Routes>
+        </>
+
+    )
+}
