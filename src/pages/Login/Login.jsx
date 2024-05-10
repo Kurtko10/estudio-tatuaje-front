f6590d07d9e8779ccbb3dc09e58d44bff074ab85
Kurtko10 <kurtkoaf@gmail.com>
2024-05-11 01:36:13 +0200

    Add: componente BUttonC , instalar redux

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,31 @@
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {CustomInput} from "../../components/CusstomInput/CustomInput";
+import { decodeToken } from "react-jwt";
+import SocialIcons from "../../components/SocialIcons/SocialIcons";
+import Button from "../../components/ButtonCita/ButtonCita";
+
+import "./Login.css";
+
+
 export const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password:""
+    });
+
+    const inputHandler =(e) =>{
+        setCredentials((prevState) =>({
+            ...prevState,
+            [e.target.name]:e.target.value,
+        }));
+    };
+
+    
+
 
 
 
