0da8ab63f6c24f4db23b68244f9a09b76327cd5b
Kurtko10 <kurtkoaf@gmail.com>
2024-05-11 08:40:15 +0200

    Edit: estilos en la vista profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -79,114 +79,232 @@ if (!token) {
   }
 
   return (
-    <>
-        <div className="justify-content-center text-center profile-container">
-            <ButtonCita className="button-cita" text="<  Pedir Cita  >" />
-            <SocialIcons urls={["https://whatsapp.com/", "https://tiktok.com/", "https://instagram.com/"]} />
-            <Form id="formProfile">
-                <div>{userData?.firstName}, aquí está tu perfil</div>
-                <Form.Group className="mb-3" controlId="formName">
-                    <Form.Label>Nombre:</Form.Label>
-                    <CustomInput
-                        typeProp="name"
-                        nameProp="firstName"
-                        placeholderProp={userData?.firstName}
-                        valueProp={userData?.firstName}
-                        isDisabled={true}
-                        disableValidation={true}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formLastname">
-                    <Form.Label>Apellidos:</Form.Label>
-                    <CustomInput
-                        typeProp="lastname"
-                        nameProp="lastName"
-                        placeholderProp={userData?.lastName}
-                        valueProp={userData?.lastName}
-                        isDisabled={true}
-                        disableValidation={false}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formEmail">
-                    <Form.Label>Email:</Form.Label>
-                    <CustomInput
-                        typeProp="email"
-                        nameProp="email"
-                        placeholderProp={userData?.email}
-                        valueProp={userData?.email}
-                        isDisabled={true}
-                        handlerProp={inputHandler}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPhone">
-                    <Form.Label>Teléfono: </Form.Label>
-                    <CustomInput
-                        typeProp="text"
-                        nameProp="phone"
-                        placeholderProp={userData?.phone || ""}
-                        valueProp={userData?.phone || ""}
-                        isDisabled={true}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formProvincia">
-                    <Form.Label>Provincia:</Form.Label>
-                    <CustomInput
-                        typeProp="text"
-                        nameProp="provincia"
-                        placeholderProp={userData?.clients?.provincia || ""}
-                        valueProp={userData?.clients?.provincia || ""}
-                        isDisabled={true}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formClientsId">
-                    <Form.Label>Num.cliente:</Form.Label>
-                    <CustomInput
-                        typeProp="text"
-                        nameProp="clientsId"
-                        placeholderProp={`Número de cliente: ${userData?.clients?.id || ""}`}
-                        valueProp={userData?.clients?.id || ""}
-                        isDisabled={true}
-                    />
-                </Form.Group>
-                <p>Datos del usuario</p>
-                {userData && (
-                    <pre>
-                        {JSON.stringify(userData, null, 2)}
-                    </pre>
-                )}
-            </Form>
-            {/* <BootstrapModal
-  show={showProfileModal}
-  handleClose={() => setShowProfileModal(false)}
-  handleSaveChanges={handleSaveChanges}
-  profileData={userData}
-  inputHandler={inputHandler}
-  token={token}
-  bringProfileHandler={bringProfileHandler} // Pasa bringProfileHandler como prop al componente BootstrapModal
-/> */}
+//     <>
+//         <div className="justify-content-center text-center profile-container">
+//             <ButtonCita className="button-cita" text="<  Pedir Cita  >" />
+//             <SocialIcons urls={["https://whatsapp.com/", "https://tiktok.com/", "https://instagram.com/"]} />
+//             <Form id="formProfile">
+//                 <div>{userData?.firstName}, aquí está tu perfil</div>
+//                 <Form.Group className="mb-3" controlId="formName">
+//                     <Form.Label>Nombre:</Form.Label>
+//                     <CustomInput
+//                         typeProp="name"
+//                         nameProp="firstName"
+//                         placeholderProp={userData?.firstName}
+//                         valueProp={userData?.firstName}
+//                         isDisabled={true}
+//                         disableValidation={true}
+//                     />
+//                 </Form.Group>
+//                 <Form.Group className="mb-3" controlId="formLastname">
+//                     <Form.Label>Apellidos:</Form.Label>
+//                     <CustomInput
+//                         typeProp="lastname"
+//                         nameProp="lastName"
+//                         placeholderProp={userData?.lastName}
+//                         valueProp={userData?.lastName}
+//                         isDisabled={true}
+//                         disableValidation={false}
+//                     />
+//                 </Form.Group>
+//                 <Form.Group className="mb-3" controlId="formEmail">
+//                     <Form.Label>Email:</Form.Label>
+//                     <CustomInput
+//                         typeProp="email"
+//                         nameProp="email"
+//                         placeholderProp={userData?.email}
+//                         valueProp={userData?.email}
+//                         isDisabled={true}
+//                         handlerProp={inputHandler}
+//                     />
+//                 </Form.Group>
+//                 <Form.Group className="mb-3" controlId="formPhone">
+//                     <Form.Label>Teléfono: </Form.Label>
+//                     <CustomInput
+//                         typeProp="text"
+//                         nameProp="phone"
+//                         placeholderProp={userData?.phone || ""}
+//                         valueProp={userData?.phone || ""}
+//                         isDisabled={true}
+//                     />
+//                 </Form.Group>
+//                 <Form.Group className="mb-3" controlId="formProvincia">
+//                     <Form.Label>Provincia:</Form.Label>
+//                     <CustomInput
+//                         typeProp="text"
+//                         nameProp="provincia"
+//                         placeholderProp={userData?.clients?.provincia || ""}
+//                         valueProp={userData?.clients?.provincia || ""}
+//                         isDisabled={true}
+//                     />
+//                 </Form.Group>
+//                 <Form.Group className="mb-3" controlId="formClientsId">
+//                     <Form.Label>Num.cliente:</Form.Label>
+//                     <CustomInput
+//                         typeProp="text"
+//                         nameProp="clientsId"
+//                         placeholderProp={`Número de cliente: ${userData?.clients?.id || ""}`}
+//                         valueProp={userData?.clients?.id || ""}
+//                         isDisabled={true}
+//                     />
+//                 </Form.Group>
+//                 {/* <p>Datos del usuario</p>
+//                 {userData && (
+//                     <pre>
+//                         {JSON.stringify(userData, null, 2)}
+//                     </pre>
+//                 )} */}
+//             </Form>
+//             {/* <BootstrapModal
+//   show={showProfileModal}
+//   handleClose={() => setShowProfileModal(false)}
+//   handleSaveChanges={handleSaveChanges}
+//   profileData={userData}
+//   inputHandler={inputHandler}
+//   token={token}
+//   bringProfileHandler={bringProfileHandler} // Pasa bringProfileHandler como prop al componente BootstrapModal
+// /> */}
 
-<Button variant="danger" onClick={() => navigate("/appointments")}>Citas</Button>
-            {/* Renderizar las tarjetas */}
-            <Row className="justify-content-center text-center">
-                <Col md={6}>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Editar perfil</Card.Title>
-                            {/* Contenido de la tarjeta para el perfil del usuario */}
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={6}>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Citas</Card.Title>
-                            {/* Contenido de la tarjeta para las citas del usuario */}
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
-                        </>
+// <Button variant="danger" onClick={() => navigate("/appointments")}>Citas</Button>
+//             {/* Renderizar las tarjetas */}
+//             <Row className="justify-content-center text-center">
+//                 <Col md={6}>
+//                     <Card>
+//                         <Card.Body>
+//                             <Card.Title>Editar perfil</Card.Title>
+//                             {/* Contenido de la tarjeta para el perfil del usuario */}
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+//                 <Col md={6}>
+//                     <Card>
+//                         <Card.Body>
+//                             <Card.Title>Citas</Card.Title>
+//                             {/* Contenido de la tarjeta para las citas del usuario */}
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+//             </Row>
+//         </div>
+//                         </>
+<>
+  <div className="justify-content-center text-center profile-container">
+    <ButtonCita className="button-cita" text="<  Pedir Cita  >" />
+    <SocialIcons urls={["https://whatsapp.com/", "https://tiktok.com/", "https://instagram.com/"]} />
+    <Form id="formProfile">
+      <div className="titleProfile">{userData?.firstName}, aquí está tu perfil</div>
+      <Row>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Nombre:</Form.Label>
+            <CustomInput
+              typeProp="name"
+              nameProp="firstName"
+              placeholderProp={userData?.firstName}
+              valueProp={userData?.firstName}
+              isDisabled={true}
+              disableValidation={true}
+            />
+          </Form.Group>
+        </Col>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formLastname">
+            <Form.Label>Apellidos:</Form.Label>
+            <CustomInput
+              typeProp="lastname"
+              nameProp="lastName"
+              placeholderProp={userData?.lastName}
+              valueProp={userData?.lastName}
+              isDisabled={true}
+              disableValidation={false}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>Email:</Form.Label>
+            <CustomInput
+              typeProp="email"
+              nameProp="email"
+              placeholderProp={userData?.email}
+              valueProp={userData?.email}
+              isDisabled={true}
+              handlerProp={inputHandler}
+            />
+          </Form.Group>
+        </Col>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formPhone">
+            <Form.Label>Teléfono:</Form.Label>
+            <CustomInput
+              typeProp="text"
+              nameProp="phone"
+              placeholderProp={userData?.phone || ""}
+              valueProp={userData?.phone || ""}
+              isDisabled={true}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formProvincia">
+            <Form.Label>Provincia:</Form.Label>
+            <CustomInput
+              typeProp="text"
+              nameProp="provincia"
+              placeholderProp={userData?.clients?.provincia || ""}
+              valueProp={userData?.clients?.provincia || ""}
+              isDisabled={true}
+            />
+          </Form.Group>
+        </Col>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formClientsId">
+            <Form.Label>Num. cliente:</Form.Label>
+            <CustomInput
+              typeProp="text"
+              nameProp="clientsId"
+              placeholderProp={`Número de cliente: ${userData?.clients?.id || ""}`}
+              valueProp={userData?.clients?.id || ""}
+              isDisabled={true}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+    </Form>
+    {/* <BootstrapModal
+//   show={showProfileModal}
+//   handleClose={() => setShowProfileModal(false)}
+//   handleSaveChanges={handleSaveChanges}
+//   profileData={userData}
+//   inputHandler={inputHandler}
+//   token={token}
+//   bringProfileHandler={bringProfileHandler} // Pasa bringProfileHandler como prop al componente BootstrapModal
+// /> */}
+    <Button variant="danger" onClick={() => navigate("/appointments")}>Citas</Button>
+    <Row className="justify-content-center text-center">
+      <Col md={6}>
+        <Card>
+          <Card.Body>
+            <Card.Title>Editar perfil</Card.Title>
+            {/* Contenido de la tarjeta para el perfil del usuario */}
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={6}>
+        <Card>
+          <Card.Body>
+            <Card.Title>Citas</Card.Title>
+            {/* Contenido de la tarjeta para las citas del usuario */}
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+</>
     );
 };
 
