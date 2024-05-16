0ce2ba05a4522bf795381a5aa26f15f6304d275c
Kurtko10 <kurtkoaf@gmail.com>
2024-05-16 22:27:04 +0200

    eliminar codigo comentado

--- src/components/ButtonCita/ButtonCita.jsx
+++ src/components/ButtonCita/ButtonCita.jsx
@@ -1,20 +1,4 @@
-// import React from "react";
-// import "./ButtonCita.css";
-// import useScrollOpacity from "../../utils/ScrollOpacityComponent";
 
-// const Button = ({ text, onClick, className }) => {
-//   const isScrolling = useScrollOpacity();
-
-//   return (
-//     <ul>
-//       <li onClick={onClick} className={`${className} ${isScrolling ? "scrolled" : ""}`}>
-//         <span>{text}</span>
-//       </li>
-//     </ul>
-//   );
-// };
-
-// export default Button;
 
 import React from "react";
 import "./ButtonCita.css";
--- src/components/CusstomInput/CustomInput.jsx
+++ src/components/CusstomInput/CustomInput.jsx
@@ -1,35 +1,3 @@
-// import { inputValidator } from "../../utils/validator";
-// import "./CustomInput.css";
-
-// export const CustomInput = ({
-//   typeProp,
-//   nameProp,
-//   placeholderProp,
-//   handlerProp,
-//   onBlurHandler,
-//   value,
-//   isDisabled,
-//   errorText,
-//   disableValidation
-// }) => {
-//   return (
-//     <div className="custom-input-container">
-//       <input
-//         className={
-//           (!disableValidation && errorText === "") ? "input-design" : "input-design input-error"
-//         }
-//         type={typeProp}
-//         name={nameProp}
-//         placeholder={placeholderProp}
-//         value={value}
-//         disabled={isDisabled}
-//         onChange={(e) => handlerProp(e)}
-//         onBlur={(e) => inputValidator(e)}
-//       />
-//       {!disableValidation && <p className="error-message">{errorText}</p>}
-//     </div>
-//   );
-// };
 
 import "./CustomInput.css";
 
